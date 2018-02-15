62942856e0f78b6e909f706cfd65d1b702590f2d
Carey Jr <cscarey@labs.neit.edu>
2018-02-15 13:59:01 -0500

    initial commit, including unity project and node app.

---
+++ socketIOProject/SocketIOProject/Assets/cameramovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramovement : MonoBehaviour
+{
+    public float speed;
+    float currentSpeed;
+    Vector3 movement = Vector3.zero;
+
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movement.y = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            movement.z = Input.GetAxis("Vertical");
+        }
+
+
+        if (Input.GetKey(KeyCode.None))
+        {
+            currentSpeed = 0;
+        }
+        else
+        {
+            currentSpeed = speed;
+        }
+        transform.Translate(movement * currentSpeed * Time.deltaTime);
+    }
+
+    
+}
---
+++ socketIOProject/SocketIOProject/Assets/network.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+
+public class network : MonoBehaviour
+{
+    static SocketIOComponent socket;
+
+    public GameObject playerPrefab;
+
+    void Start()
+    {
+        socket = GetComponent<SocketIOComponent>();
+        socket.On("open", OnConnected);
+        socket.On("spawn player", OnSpawn);
+    }
+
+    void OnConnected(SocketIOEvent e)
+    {
+        Debug.Log("We are connected.");
+
+        socket.Emit("playerhere");
+
+    }
+
+    void OnSpawn(SocketIOEvent e)
+    {
+        Debug.Log("Player spawned.");
+        Instantiate(playerPrefab);
+    }
+}
