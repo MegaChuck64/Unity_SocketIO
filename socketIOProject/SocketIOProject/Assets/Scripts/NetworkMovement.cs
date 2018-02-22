aa42888c732e70da55886d6f6539d19e7d8bcde5
Carey Jr <cscarey@labs.neit.edu>
2018-02-22 15:36:33 -0500

    got movement working between networks

---
+++ socketIOProject/SocketIOProject/Assets/Scripts/NetworkMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class NetworkMovement : MonoBehaviour
+{
+    Vector3 pos;
+    public SocketIOComponent socket;
+
+
+    void FixedUpdate()
+    {
+        pos = transform.position;
+        OnMove(pos);
+    }
+
+    public void OnMove(Vector3 position)
+    {
+        Debug.Log("sending pos to server " + VectorToJson(position));
+        socket.Emit("move", new JSONObject(VectorToJson(position)));
+    }
+
+
+    string VectorToJson(Vector3 vector)
+    {
+        return string.Format(@"{{""x"":""{0}"", ""y"":""{1}""}}", vector.x, vector.z);
+    }
+}
+
+
---
+++ socketIOProject/SocketIOProject/Assets/Scripts/network.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+
+public class network : MonoBehaviour
+{
+    static SocketIOComponent socket;
+
+    public GameObject playerPrefab;
+
+    Dictionary<string, GameObject> players;
+
+    void Start()
+    {
+        socket = GetComponent<SocketIOComponent>();
+        socket.On("open", OnConnected);
+        socket.On("spawn player", OnSpawn);
+        socket.On("disconnected", OnDisconnected);
+        socket.On("move", OnMove);
+        players = new Dictionary<string, GameObject>();
+    }
+
+    void OnConnected(SocketIOEvent e)
+    {
+        Debug.Log("We are connected.");
+
+       // socket.Emit("playerhere");
+
+    }
+
+    void OnSpawn(SocketIOEvent e)
+    {
+        Debug.Log("Player spawned.");
+        var player = Instantiate(playerPrefab);
+        players.Add(e.data["id"].ToString(), player);
+        Debug.Log("count: " + players.Count);
+    }
+
+    void OnDisconnected(SocketIOEvent e)
+    {
+        Debug.Log("Player disconnected: " + e.data);
+
+        var id = e.data["id"].ToString();
+
+        var player = players[id];
+
+        Destroy(player);
+        players.Remove(id);
+    }
+
+    void OnMove(SocketIOEvent e)
+    {
+       Debug.Log("Networked player is moving " + e.data);
+        var id = e.data["id"].ToString();
+        var player = players[id];
+        var pos = new Vector3(GetFloatFromJson(e.data, "x"), 0, GetFloatFromJson(e.data, "y"));
+        Debug.Log("pos: " + pos);
+
+        var netMove = player.GetComponent<CharacterMovement>();
+           
+        netMove.NetworkMovement(pos);
+
+        
+    }
+
+    float GetFloatFromJson(JSONObject data, string key)
+    {
+        return float.Parse(data[key].ToString().Replace("\"", ""));
+    }
+}
